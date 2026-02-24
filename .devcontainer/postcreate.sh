cdd414625c7531a486b9046cadfb9dfc39942288
Jarrett Booz <89405171+sei-jbooz@users.noreply.github.com>
2026-02-24 11:29:37 -0500

    Remove broken chown (#154)

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,7 +3,6 @@
 # Show git dirty status in zsh prompt
 git config devcontainers-theme.show-dirty 1
 
-sudo chown -R $(whoami): /home/vscode/.microsoft
 sudo chown -R $(whoami): /home/vscode/.claude
 
 # oh-my-zsh plugins
